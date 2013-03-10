7e01b1ef99436051aa55c6f84141310375e61c3e
Bryan Mayjor <bmayjor@gmail.com>
2013-03-10 10:09:12 -0700

    Update gozulla.js
    added populate menu

--- js/gozulla.js
+++ js/gozulla.js
@@ -66,6 +66,24 @@ $(document).ready(function() {
     $('#block-system-main-menu ul li.menu-item-893 a').attr('href')+location.search);
     $('#block-system-main-menu ul li.menu-item-891 a').attr('href',
     $('#block-system-main-menu ul li.menu-item-891 a').attr('href')+location.search);
+    
+    // popuate menu links
+    $('#block-menu-menu-populate-menu ul li.menu-item-3339 a').attr('href',
+    $('#block-menu-menu-populate-menu ul li.menu-item-3339 a').attr('href')+location.search);
+    $('#block-menu-menu-populate-menu ul li.menu-item-3340 a').attr('href',
+    $('#block-menu-menu-populate-menu ul li.menu-item-3340 a').attr('href')+location.search);
+    $('#block-menu-menu-populate-menu ul li.menu-item-3341 a').attr('href',
+    $('#block-menu-menu-populate-menu ul li.menu-item-3341 a').attr('href')+location.search);
+    $('#block-menu-menu-populate-menu ul li.menu-item-3342 a').attr('href',
+    $('#block-menu-menu-populate-menu ul li.menu-item-3342 a').attr('href')+location.search);
+    $('#block-menu-menu-populate-menu ul li.menu-item-3343 a').attr('href',
+    $('#block-menu-menu-populate-menu ul li.menu-item-3343 a').attr('href')+location.search);
+    $('#block-menu-menu-populate-menu ul li.menu-item-3344 a').attr('href',
+    $('#block-menu-menu-populate-menu ul li.menu-item-3344 a').attr('href')+location.search);
+    $('#block-menu-menu-populate-menu ul li.menu-item-3345 a').attr('href',
+    $('#block-menu-menu-populate-menu ul li.menu-item-3345 a').attr('href')+location.search);
+    $('#block-menu-menu-populate-menu ul li.menu-item-3346 a').attr('href',
+    $('#block-menu-menu-populate-menu ul li.menu-item-3346 a').attr('href')+location.search);
   }
 
   $(document.getElementById('findme-link').href = location.href.substring(0,location.href.indexOf('?')));
